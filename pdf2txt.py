b9a8af72b3b4f79ff2a0166cb9036e41ba674432
bricaud <benjamin.ricaud@gmail.com>
2017-02-06 14:06:21 +0100

    added path as an argument of the program

--- pdf2txt.py
+++ pdf2txt.py
@@ -1,14 +1,25 @@
-#!/usr/bin/env python
-import os
-import subprocess
-import glob
-import platform
+#!/usr/bin/env python3
 """ Program that convert a pdf to a text file using Tesseract OCR.
 
 	The pdf file is first converted to a png file using ghostscript,
 	then the png file if processed by Tesseract.
 
 """
+import os
+import subprocess
+import glob
+import platform
+import argparse
+
+parser = argparse.ArgumentParser(description='Convert pdf files to txt files in the given folder.')
+parser.add_argument('folder',
+                   help='folder where the pdf files are stored')
+
+args = parser.parse_args()
+input_dic = vars(args)
+print(input_dic)
+PDF_PATH = input_dic['folder'] 
+#PDF_PATH = '/media/benjamin/Elements/pdfs/'
 
 def png_to_txt(pngpath,short_name,txtpath,log_file):
 	""" Extract the text from a set of png files.
@@ -31,7 +42,7 @@ def png_to_txt(pngpath,short_name,txtpath,log_file):
 				with open(log_file, 'a') as logfile:
 					logfile.write('Error with file: {}\n'.format(filename))  # report errors
 			else:
-				print('Text extracted form file: {}\n'.format(filename))
+				print('Text extracted form file: {}'.format(filename))
 		except:
 			print('error extracting text with file {}'.format(filename))
 			with open(log_file, 'a') as logfile:
@@ -51,7 +62,7 @@ def pdf_to_png(pdf_file,short_name,png_path,page_limit=4):
 	proc_results = subprocess.run(cmd_pdf2png.split(), stdout=subprocess.PIPE,timeout=60)
 	return proc_results
 
-PDF_PATH = '/media/benjamin/Elements/pdfs/'
+#PDF_PATH = '/media/benjamin/Elements/pdfs/'
 LOG_FILE1 = 'logfile_pdf2png.txt'
 LOG_FILE2 = 'logfile_png2txt.txt'
 
--- text_extractor.py
+++ text_extractor.py
@@ -2,6 +2,20 @@
 """ Extract the texts from files in the specified path and save them in pickle file (dataframe).
 """
 import textbox
-TXT_PATH = '/media/benjamin/Elements/pdfs/txt'
-PICKLE_FILE = './texts3.pkl'
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Extract the texts from files in the specified path and save them in pickle file (dataframe).')
+parser.add_argument('folder',
+                   help='folder where the pdf files are stored')
+
+args = parser.parse_args()
+input_dic = vars(args)
+print(input_dic)
+TXT_PATH = os.path.join(input_dic['folder'],'txt')
+#TXT_PATH = '/media/benjamin/Elements/pdfs/txt'
+PICKLE_PATH = os.path.join(input_dic['folder'],'pickle')
+if not os.path.exists(PICKLE_PATH):
+    os.makedirs(PICKLE_PATH)
+PICKLE_FILE = os.path.join(PICKLE_PATH,'texts3.pkl')
 textbox.auto_extract(TXT_PATH,PICKLE_FILE)
